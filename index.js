a9d20a890e53187dbab4078511090a5f5b8b5a7e
Aryan Chavan <aryanchavan2003@gmail.com>
2021-01-20 13:12:28 +0530

    render and serve ejs template

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("hello, world!");
+// envinronment variables
+require("dotenv").config();
+
+// requiring node modules
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+
+// configuring app and middleware
+const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+// globals
+let clip = "hello, world!";
+
+// listen
+app.listen(process.env.PORT, () => {
+	console.log(`listening on port ${process.env.PORT}`);
+});
+
+// serving the page
+app.get("/", (req, res) => {
+	res.render("index", { clip: clip });
+});
